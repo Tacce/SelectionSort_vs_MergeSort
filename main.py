733229d61a7656a6c14e6d8371ea0c5bacf84bf8
Tacce <102242997+Tacce@users.noreply.github.com>
2023-04-03 18:56:02 +0200

    PlotSortGraph function added

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import sys
+import matplotlib.pyplot as plt
+import numpy as np
+import time
+import random
 
 inf = sys.maxsize
 
@@ -12,10 +16,10 @@ def MergeSort(A, p, r):
 
 
 def Merge(A, p, q, r):
-    L = A[p:q + 1]
-    R = A[q + 1:r + 1]
-    L.append(inf)
-    R.append(inf)
+    L = np.array(A[p:q + 1])
+    R = np.array(A[q + 1:r + 1])
+    L = np.append(L, inf)
+    R = np.append(R, inf)
     i = 0
     j = 0
     for k in range(p, r + 1):
@@ -37,11 +41,27 @@ def SelectionSort(A):
         A[i], A[minimum] = A[minimum], A[i]
 
 
+def plotSortGraph(algType, insertType):
+    x, y = [], []
+    for i in range(10, 1000, 10):
+        x.append(i)
+        A = np.arange(i) if insertType == 0 else random.sample(range(i), i)
+        if algType == 0:
+            start = time.perf_counter()
+            SelectionSort(A)
+            end = time.perf_counter()
+        else:
+            start = time.perf_counter()
+            MergeSort(A, 0, len(A) - 1)
+            end = time.perf_counter()
+        z = y[-1] if (len(y) != 0) else 0
+        y.append((end - start) / i + z)
+    plt.plot(x, y)
+    title = 'Selection-Sort' if algType == 0 else 'Merge-Sort'
+    title += ' on Ordered List' if algType == 0 else ' on Randomized List'
+    plt.title(title)
+    plt.show()
 
 
 if __name__ == '__main__':
-    A = [1, 4, 2, 5, 9, 3]
-    print(A)
-    MergeSort(A, 0, len(A) - 1)
-    #SelectionSort(A)
-    print(A)
+    [[plotSortGraph(i, j) for i in range(2)] for j in range(2)]
